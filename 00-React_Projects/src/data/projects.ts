f8d92d9793940d5f165224240e88690de512fd55
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-04-02 22:28:55 -0500

    Add Data Object

---
+++ 00-React_Projects/src/data/projects.ts
@@ -0,0 +1,10 @@
+import { ProjectType } from "../types";
+
+export const projects: ProjectType[] = [
+    {
+        projectName: "Birthday Buddy",
+        section: "Fundamental",
+        imageSrc: "",
+        link: ""
+    },
+];
