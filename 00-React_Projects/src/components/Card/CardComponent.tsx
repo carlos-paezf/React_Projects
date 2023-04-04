28f09c2ce07737e2e21fc9977bf510ed2e846ed5
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-04-03 20:02:00 -0500

    Add Load State

--- 00-React_Projects/src/components/Card/CardComponent.tsx
+++ 00-React_Projects/src/components/Card/CardComponent.tsx
@@ -1,11 +1,13 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ProjectType } from "../../types";
 import './Card.css';
 
 export const CardComponent: FC<ProjectType> = ( { imageSrc, projectName, link } ) => {
+    const [ loaded, setLoaded ] = useState( false );
+
     return (
         <div className="card">
-            <img className="project-image" src={ imageSrc } alt={ projectName } />
+            <img className={ `project-image ${ !loaded && 'loading-blur' }` } src={ imageSrc } alt={ projectName } onLoad={ () => setLoaded( true ) } />
             <a className="project-link" href={ link } target="_blank" rel="noopener noreferrer">{ projectName }</a>
         </div>
     );
