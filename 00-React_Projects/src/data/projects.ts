3354aacc8df1aa31e3046b8497a000428cff5cef
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-04-03 19:47:00 -0500

    Update Projects Data

--- 00-React_Projects/src/data/projects.ts
+++ 00-React_Projects/src/data/projects.ts
@@ -1,10 +1,12 @@
 import { ProjectType } from "../types";
 
+const baseURL = ( projectId: string ) => `https://github.com/carlos-paezf/React_Projects/blob/main/00-React_Projects/src/assets/${ projectId }.png?raw=true`;
+
 export const projects: ProjectType[] = [
     {
         projectName: "Birthday Buddy",
         section: "Fundamental",
-        imageSrc: "",
+        imageSrc: baseURL( '01' ),
         link: ""
     },
 ];
