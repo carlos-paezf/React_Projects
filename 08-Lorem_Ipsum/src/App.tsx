7339bb275dabd41d588451a5c91b818e32da0954
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-04-11 13:47:52 -0500

    Update 08-Lorem_Ipsum

--- 08-Lorem_Ipsum/src/App.tsx
+++ 08-Lorem_Ipsum/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from "react";
+import { textBase } from '../data/index';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+    const [ count, setCount ] = useState<number>( 0 );
+    const [ text, setText ] = useState<string[]>( [] );
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const handleSubmit = ( e: FormEvent ) => {
+        e.preventDefault();
+
+        let amount = count;
+
+        if ( amount <= 0 ) { amount = 1; }
+
+        if ( amount > 8 ) { amount = 8; }
+
+        setText( textBase.slice( 0, amount ) );
+    };
+
+    return (
+        <section className="section-center">
+            <h3>Tired of boring lorem ipsum?</h3>
+
+            <form className="lorem-form" onSubmit={ handleSubmit }>
+                <label htmlFor="amount">Paragraphs:</label>
+                <input type="number" id="number"
+                    min={ 0 } max={ 8 }
+                    name="amount" value={ count }
+                    onChange={ ( e ) => setCount( parseInt( e.target.value ) ) } />
+            </form>
+
+            <article className="lorem-text">
+                {
+                    text.map( ( item, index ) => <p key={ index }>{ item }</p> )
+                }
+            </article>
+        </section>
+    );
 }
 
-export default App
+export default App;
