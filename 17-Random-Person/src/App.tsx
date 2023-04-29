9cf135dab6afce33d0c4e728cf888b1c61753267
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-04-28 20:25:15 -0500

    Update 17-Random-Person

--- 17-Random-Person/src/App.tsx
+++ 17-Random-Person/src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MouseEventHandler, useEffect, useState } from "react";
+import { FaCalendarTimes, FaEnvelopeOpen, FaLock, FaMap, FaPhone, FaUser } from 'react-icons/fa';
+import { IPerson } from "./types";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const url = 'https://randomuser.me/api/';
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
+
+
+function App () {
+    const [ loading, setLoading ] = useState( true );
+    const [ person, setPerson ] = useState<IPerson>();
+    const [ value, setValue ] = useState( 'random person' );
+    const [ title, setTitle ] = useState( 'name' );
+
+    const getPerson = async () => {
+        setLoading( true );
+
+        const response = await fetch( url );
+        const data = await response.json();
+        const person = data.results[ 0 ];
+
+        const { phone, email, dob: { age } } = person;
+        const { large: image } = person.picture;
+        const { password } = person.login;
+        const { first, last } = person.name;
+        const { street: { number, name } } = person.location;
+
+        const newPerson = {
+            image, phone, email, password, age,
+            street: `${ number } ${ name }`,
+            name: `${ first }, ${ last }`
+        };
+
+        setPerson( newPerson );
+        setLoading( false );
+        setTitle( 'name' );
+        setValue( newPerson.name );
+    };
+
+    useEffect( () => {
+        getPerson();
+    }, [] );
+
+    const handleValue: MouseEventHandler<HTMLButtonElement> = ( event ) => {
+        if ( event.target.classList.contains( 'icon' ) ) {
+            const newValue = event.target.dataset.label;
+            setTitle( newValue );
+            setValue( person[ newValue ] );
+        }
+    };
+
+    return (
+        <main>
+            <div className="block bcg-black"></div>
+            <div className="block">
+                <div className="container">
+                    <img src={ ( person && person.image ) || defaultImage } alt="random user" className="user-img" />
+
+                    <p className="user-title">My { title }</p>
+                    <p className="user-value">{ value }</p>
+
+                    <div className="values-list">
+                        <button className="icon" data-label="name" onMouseOver={ handleValue }><FaUser /></button>
+                        <button className="icon" data-label="email" onMouseOver={ handleValue }><FaEnvelopeOpen /></button>
+                        <button className="icon" data-label="age" onMouseOver={ handleValue }><FaCalendarTimes /></button>
+                        <button className="icon" data-label="street" onMouseOver={ handleValue }><FaMap /></button>
+                        <button className="icon" data-label="phone" onMouseOver={ handleValue }><FaPhone /></button>
+                        <button className="icon" data-label="password" onMouseOver={ handleValue }><FaLock /></button>
+                    </div>
+
+                    <button type="button" className="btn" onClick={ getPerson } disabled={ loading }>
+                        { loading ? 'loading...' : 'random user' }
+                    </button>
+                </div>
+            </div>
+        </main>
+    );
 }
 
-export default App
+
+export default App;
---
+++ 17-Random-Person/src/types/index.ts
@@ -0,0 +1,9 @@
+export interface IPerson {
+    image: string;
+    phone: string;
+    email: string;
+    password: string;
+    age: string;
+    street: string;
+    name: string;
+}
